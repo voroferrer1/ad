30829b573acc4df97171f7a83eb04f6c5d43422c
voroferrer <2dam.vferrer@institutoserpis.org>
2018-09-14 10:29:15 +0200

    Actualizado CMySQL

--- CMySQL/CMySQL/Program.cs
+++ CMySQL/CMySQL/Program.cs
@@ -1,15 +1,27 @@
 ﻿using System;
 using MySql.Data.MySqlClient;
+using System.Data;
 namespace CMySQL
 {
     class MainClass
     {
+		private static string[] getFieldNames(){
+			
+		}
         public static void Main(string[] args)
         {
-			MySqlConnection mySqlConnection = new MySqlConnection(
+			IDbConnection dbConnection = new MySqlConnection(
 				"server=localhost;database=dbprueba;user=root;password=sistemas; ssl-mode=none;");
-			mySqlConnection.Open();
-			mySqlConnection.Close();
+			dbConnection.Open();
+
+			IDbCommand dbCommand = dbConnection.CreateCommand();
+			dbCommand.CommandText = "select * from categoria order by id";
+			IDataReader dataReader = dbCommand.ExecuteReader();
+			while (dataReader.Read()){
+				Console.WriteLine("id={0} nombre{1}",dataReader["id"],dataReader["nombre"]);
+			}
+			dataReader.Close();
+			dbConnection.Close();
         }
     }
 }
